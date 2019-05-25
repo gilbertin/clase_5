cc56fb0015bbaee95b4fd9f535a1caf75c91b5ff
Gilberto Javier Morales Hernandez <gilberto morales@99.200-57-1.bestelclientes.com.mx>
2019-05-24 21:51:26 -0500

    add js file nivel.js

---
+++ nivel.js
@@ -0,0 +1,10 @@
+const Nivel = parseInt('9')
+                    if Nivel <= 9
+                        console.log('Es Aprendiz')
+                    }elif {
+                        Nivel >= 10 && <= 19
+                        console.log('Es Acrobata') 
+                    }elif {
+                        Nivel >=20
+                        console.log('Es Ninja')
+                    }
