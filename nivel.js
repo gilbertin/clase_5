41089b3a48b03b528fba0a2eac70da552a52b1c2
Gilberto Javier Morales Hernandez <gilberto morales@99.200-57-1.bestelclientes.com.mx>
2019-05-27 18:21:51 -0500

    add some node scripts

--- nivel.js
+++ nivel.js
@@ -1,10 +1,8 @@
-const Nivel = parseInt('9')
-                    if Nivel <= 9
-                        console.log('Es Aprendiz')
-                    }elif {
-                        Nivel >= 10 && <= 19
-                        console.log('Es Acrobata') 
-                    }elif {
-                        Nivel >=20
-                        console.log('Es Ninja')
-                    }
+const nivel = parseInt('21')
+    if (nivel <= 9) {
+        console.log('Es Aprendiz')
+    }else if (nivel >= 10 && nivel <= 19) {
+        console.log('Es Acrobata') 
+    }else if (nivel >=20) {
+        console.log('Es Ninja')
+    }
