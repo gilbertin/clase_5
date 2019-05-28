57a65bef8c55d523c6c10681aacff11b07b9afdd
Gilberto Javier Morales Hernandez <gilberto morales@99.200-57-1.bestelclientes.com.mx>
2019-05-27 21:30:06 -0500

    add some files

---
+++ multiplicador.js
@@ -0,0 +1,20 @@
+
+var i = 1;
+let n = 10;
+
+
+// for ( i = 1; i <= n; i++){
+//     console.log(n + ' x ' + i + ' = ' +(n * i));
+// }
+
+//while (i <= 10) {
+//    console.log(n + ' x ' + i + ' = ' +(n * i));
+//    i++
+//}
+
+do {
+    console.log(n + ' x ' + i + ' = ' +(n * i));
+    i++;
+}while (i <= n)
+
+
---
+++ travesuras.js
@@ -0,0 +1,13 @@
+// Paco hizo travesuras en el salón y la maestra lo castigó, para poder salir tiene escribir en el 
+// pizarrón todos los números del num1 al num2, ¿puedes ayudarle a
+// Paco a hacer un algoritmo que haga esto por el
+
+
+
+var i = 8;
+let n = 10;
+
+for ( i ; i <= n; i++){
+    console.log(' numero ' + i);
+}
+
