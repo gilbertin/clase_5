8d8df643d93c59a6a6e92198f07ad3c8f8362ddf
Gilberto Javier Morales Hernandez <gilberto morales@99.200-57-1.bestelclientes.com.mx>
2019-05-28 18:39:24 -0500

    modified file names

--- hambre1.js
+++ hambre1.js
@@ -1,6 +1,22 @@
+// Cuando tengo hambre estoy triste, si “hambre” es un valor booleano (true o false), tienes que descifrar si estoy feliz o tengo hambre
+
+
+// INICIO
+// 	LEER si tiene hambre?
+// 	SI hambre == true ENTONCES
+// 		ESCRIBE “Tengo Hambre”
+// 	SINO
+// 		ESCRIBE “Estoy Feliz”
+// 	FIN-SI
+// FIN
+
+
+
 const hambre = Boolean('no')
     if (hambre == String 'no') {
         console.log('Estoy Triste')
     }else (hambre == String 'no') {
         console.log('Estoy Feliz')
     }
+
+    
