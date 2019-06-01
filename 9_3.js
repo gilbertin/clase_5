fbaab05088949b6050cd7104a09484d72bd05dfa
Gilberto Javier Morales Hernandez <gilberto morales@99.200-57-1.bestelclientes.com.mx>
2019-05-31 20:44:16 -0500

    add changes

--- 9_3.js
+++ 9_3.js
@@ -1,2 +1,3 @@
-Paco no estudió mucho para sus últimos exámenes, quiere saber cuántos exámenes tendrá que realizar de nuevo, 
-el programa recibirá la cantidad de exámenes y un arreglo de calificaciones para poder calcular el resultado
+// Paco no estudió mucho para sus últimos exámenes, quiere saber cuántos exámenes tendrá que realizar de nuevo, 
+// el programa recibirá la cantidad de exámenes y un arreglo de calificaciones para poder calcular el resultado
+
